21cb587a26f4e4103988acb73f18a8cf46f2d57d
IT18Monkey <15151967329@163.com>
2017-05-15 22:26:05 +0800

    添加idea插件

--- src/main/java/club/lkdteam/generator/Generator.java
+++ src/main/java/club/lkdteam/generator/Generator.java
@@ -17,7 +17,7 @@ import java.util.List;
  * Created by whh on 2017/5/14.
  */
 public class Generator {
-    public  void generate() throws Exception{
+    public void generate() throws Exception {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         ConfigurationParser cp = new ConfigurationParser(warnings);
@@ -29,5 +29,14 @@ public class Generator {
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) {
+        Generator generator = new Generator();
+        try {
+            generator.generate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
